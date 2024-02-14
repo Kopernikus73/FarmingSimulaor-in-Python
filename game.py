6bb6bc0b7f3a77755d349b88bef6a86e25fba32a
Kopernikus <128999240+Kopernikus73@users.noreply.github.com>
2024-02-14 18:20:42 +0100

    Update game.py

--- game.py
+++ game.py
@@ -2,14 +2,16 @@ import savestate
 import input
 
 import Abkürzungen as A
-    
+
+
+
 def Game():
     A.p("Welcome to Farming Simulator!\n","bold")
     input.getIn('Type "help" to get and overview of all commands',0)
-    while input.game:
+    while input.game_on:
         input.getIn("",0)
 
-    
+
 if __name__ == "__main__":
-    savestate.readData(r'fieldsave.txt')
+    savestate.readData(r'saves\fieldsave.txt',r'saves\invsave.txt',r'saves\moneysave.txt')
     Game()
