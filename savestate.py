3dc5e8862a92e53e1daae1c1eb68cd81abe40d51
Kopernikus <128999240+Kopernikus73@users.noreply.github.com>
2024-02-19 00:11:25 +0100

    Alpha0.3 Update

--- savestate.py
+++ savestate.py
@@ -3,7 +3,7 @@ import Abkürzungen as A
 IN = ["","",""]
 inv = []
 money = 0
-orgText = ["Aa0Ba0Ca0Da0","","0"]
+orgText = ["A/01B/01C/01D/01","4w2p","0"]
 
 def insertCharacter(file_path : str, position : int, character : int):
     with open(file_path, 'r') as f:
@@ -18,17 +18,35 @@ def readData(file_path : str, file_path2 : str,file_path3 : str):
     global fielddata
     global invdata
     global moneydata
-    IN[0] = input("Spielstand laden?\n y/n\n : ")
+    IN[0] = input('Load savestate? //broken//   y/n\n : ')
     
     
     if IN[0] == "y":
-        with open(file_path,'r') as f:
-            fielddata = str(f.read())
-        with open(file_path2,'r') as f:
-            invdata = str(f.read())
-        with open(file_path3,'r') as f:
-            moneydata = str(f.read())
-        A.p("Spielstand geladen!\n","italic")
+        A.p("savestate could not be loaded")
+        with open(file_path,'w') as f:
+            f.write(orgText[0])
+            fielddata = orgText[0]
+        with open(file_path2,'w') as f:
+            f.write(orgText[1])
+            invdata = orgText[1]
+            for i in range(int(len(invdata)/2)):
+                inv.append(invdata[i*2] + invdata[i*2+1])
+        with open(file_path3,'w') as f:
+            f.write(orgText[2])
+            moneydata = orgText[2]
+            
+        #A.p("Savestate deleted!\n","italic")
+        # with open(file_path,'r') as f:
+        #     fielddata = str(f.read())
+        # with open(file_path2,'r') as f:
+        #     invdata = str(f.read())
+        #     for i in range(int(len(invdata)/2)):
+        #         inv.append(invdata[i*2] + invdata[i*2+1])
+        # with open(file_path3,'r') as f:
+        #         moneydata = str(f.read())
+        # A.p("Savestate loaded`!\n","italic")
+        
+        #Strg+K  +  Strg+U -> Kommentar entfernen
     
     elif IN[0] == "n":
         with open(file_path,'w') as f:
@@ -37,17 +55,17 @@ def readData(file_path : str, file_path2 : str,file_path3 : str):
         with open(file_path2,'w') as f:
             f.write(orgText[1])
             invdata = orgText[1]
-            for i in invdata:
-                inv.append(i)
+            for i in range(int(len(invdata)/2)):
+                inv.append(invdata[i*2] + invdata[i*2+1])
         with open(file_path3,'w') as f:
             f.write(orgText[2])
             moneydata = orgText[2]
             
-        A.p("Spielstand gelöscht!\n","italic")
+        A.p("Savestate deleted!\n","italic")
             
     elif IN[0] != "y" or IN[0] != "n":
         error(IN[0])
         readData(file_path,file_path2,file_path3)
     
 def error(i):
-    A.p(f'ungültige Eingabe >>> {i} <<<\n',"italic")
+    A.p(f'input error >>> {i} <<<\n',"italic")
